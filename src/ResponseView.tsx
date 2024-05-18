7ee01db84243d52f3025ac7368880f65b6f66ca6
Septias <scoreplayer2000@gmail.com>
2024-05-18 14:19:46 +0200

    sticky auto-scroll

--- src/ResponseView.tsx
+++ src/ResponseView.tsx
@@ -1,4 +1,4 @@
-import { Accessor, For, type Component } from 'solid-js';
+import { Accessor, For, createEffect, type Component } from 'solid-js';
 import { PingData } from './App';
 import { format } from 'date-fns';
 interface ChatProps {
@@ -9,10 +9,30 @@ interface ChatProps {
 }
 
 const Chats: Component<ChatProps> = ({ name, data, color, active }) => {
+
+  let element;
+  let looking = true;
+  createEffect(() => {
+    data.length;
+    if (looking) {
+      element.scrollTop = element.scrollHeight;
+    }
+  })
+
+  function scroll_handler() {
+    console.log(element.scrollTop, element.scrollHeight);
+
+    if (element.scrollTop > element.scrollHeight - 420) {
+      looking = true;
+    } else {
+      looking = false;
+    }
+  }
+
   return (
     <div style={`width: 300px`}>
       <h1 style={`color: ${color}`} class="ml-1 font-bold font-mono case-capital text-xl"> {name} </h1>
-      <div style={`border-color: ${color}; max-height: 500px`} class="font-mono border-2 rounded-md p-4 py-1 overflow-y-auto">
+      <div ref={element} onScroll={scroll_handler} style={`border-color: ${color}; max-height: 400px`} class="font-mono border-2 rounded-md p-4 py-1 overflow-y-auto">
         <For each={data}>
           {ping => (
             <div class="flex gap-2">
